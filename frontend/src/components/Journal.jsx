6e18e4636a5212846492a6cd62cfee0ee8068f31
MUSTAPHA SAID <mustaphasaid.pr@gmail.com>
2022-10-26 17:10:33 +0200

    css journal v1 done

--- frontend/src/components/Journal.jsx
+++ frontend/src/components/Journal.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/button-has-type */
 function Journal() {
   return (
-    <>
-      <div className="Journal" />
-      <span>hello</span>
-    </>
+    <div className="Journal">
+      <span> Tell me more : </span>
+      <textarea rows="13" column="10" placeholder="type to add a note..." />
+      <div clasName="noteFooter">
+        <button className="save">save</button>
+      </div>
+    </div>
   );
 }
 
