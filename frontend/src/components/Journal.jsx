65700d6320b3d190d126da6a80470987e8fb15c3
MUSTAPHA SAID <mustaphasaid.pr@gmail.com>
2022-10-28 10:46:39 +0200

    timeline started

--- frontend/src/components/Journal.jsx
+++ frontend/src/components/Journal.jsx
@@ -1,11 +1,30 @@
 /* eslint-disable react/button-has-type */
+
+import { useState } from "react";
+
 function Journal() {
+  const [noteText, setnoteText] = useState("");
+
+  function handlechange(event) {
+    setnoteText(event.target.value);
+  }
+
+  function handleSaveClick() {}
+
   return (
     <div className="Journal">
       <span> Tell me more : </span>
-      <textarea rows="13" column="10" placeholder="type to add a note..." />
+      <textarea
+        rows="13"
+        column="10"
+        placeholder="type to add a note..."
+        value={noteText}
+        onChange={handlechange}
+      />
       <div clasName="noteFooter">
-        <button className="save">save</button>
+        <button className="save" onClick={handleSaveClick}>
+          save
+        </button>
       </div>
     </div>
   );
