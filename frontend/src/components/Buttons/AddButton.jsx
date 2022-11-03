d309725df723d6a975eb793659ea2897afffefe2
Laynaria <pro.laynaria@outlook.fr>
2022-11-03 16:39:19 +0100

    Finished debugging so everyone can go back to code!

--- frontend/src/components/Buttons/AddButton.jsx
+++ frontend/src/components/Buttons/AddButton.jsx
@@ -4,9 +4,9 @@ import addButton from "@assets/buttons/addToast.png";
 export default function AddButton() {
   return (
     <div className="navButton">
-      <p className="addButtonBackground"> </p>
-      <img src={addButton} alt="Add your mood " className="addButton" />
-      <p> </p>
+      <p className="addButtonBackground">
+        <img src={addButton} alt="Add your mood " className="addButton" />
+      </p>
     </div>
   );
 }
