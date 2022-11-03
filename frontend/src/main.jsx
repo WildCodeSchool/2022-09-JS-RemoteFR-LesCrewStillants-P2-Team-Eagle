5ec0e4fb7a5d5dbfc63853f76aa9677779a8c7c4
Alicia-Dupil <dupil.alicia@gmail.com>
2022-11-03 11:45:17 +0100

    Add debugDev

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppNav from "./AppNav";
-import ReactDOM from "react-dom";
+
 import App from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <AppNav />
-  </React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
