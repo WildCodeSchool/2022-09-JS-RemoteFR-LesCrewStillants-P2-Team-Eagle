19031879a8665d9655c8b0e9a9dd3f11414108ec
Laynaria <pro.laynaria@outlook.fr>
2022-11-04 09:42:15 +0100

    fixed some css issues mainly and made the pop up open from navbar!

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Home from "@pages/HomePopUp";
 import TodoList from "@components/Home/ToDoList/TodoList";
 
 export default function HomePage({ clickedButtonHome }) {
@@ -7,8 +6,6 @@ export default function HomePage({ clickedButtonHome }) {
       id="HomePage"
       className={clickedButtonHome ? "homeVisible" : "homeHidden"}
     >
-      <Home />
-
       <TodoList />
     </section>
   );
