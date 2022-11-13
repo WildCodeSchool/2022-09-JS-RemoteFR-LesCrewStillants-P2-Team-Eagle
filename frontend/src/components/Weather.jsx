efc6fbceb9ac78522bbbc4adf2f3e3bc7e1de309
Laynaria <pro.laynaria@outlook.fr>
2022-11-13 20:09:26 +0100

    Wrote some weather tips and changed the switch cases to ifs cause it wasn't refreshing like it should when weather changes.

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -9,15 +9,48 @@ export default function Weather() {
   const [backgroundPicture, setBackgroundPicture] = useState("");
 
   const weatherExtra = [
-    { weatherTip: "tip 1", backgroundPicture: "./src/assets/MoodVeryGood.png" },
-    { weatherTip: "tip 2", backgroundPicture: "" },
-    { weatherTip: "tip 3", backgroundPicture: "" },
-    { weatherTip: "tip 4", backgroundPicture: "" },
-    { weatherTip: "tip 5", backgroundPicture: "" },
-    { weatherTip: "tip 6", backgroundPicture: "" },
-    { weatherTip: "tip 7", backgroundPicture: "" },
-    { weatherTip: "tip 8", backgroundPicture: "" },
-    { weatherTip: "tip 9", backgroundPicture: "" },
+    {
+      weatherTip:
+        "Sunshine is everywhere! Maybe you should take your sunglasses!",
+      backgroundPicture: "./src/assets/MoodVeryGood.png",
+    },
+    {
+      weatherTip:
+        "There are just few clouds today! But sun rays are still there, don't let them deceives you!",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip:
+        "Half clouds, half sun. With such diversity nothing should happen today.",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip:
+        "Clouds... Clouds everywhere! It may rains if the clouds are dark!",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip:
+        "There's a bit of rain, but normally not too much. Covering your head should be enough.",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip: "It's raining today! You should take an umbrella!",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip: "There's a thunderstorm out there! Take care of lightning!",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip:
+        "Snow is falling today! You should put warm clothes and maybe do a snowman!",
+      backgroundPicture: "",
+    },
+    {
+      weatherTip: "It's foggy today, pay more attention than ever!",
+      backgroundPicture: "",
+    },
   ];
 
   // sample weather, in case nothing load
@@ -54,46 +87,68 @@ export default function Weather() {
   }, []);
 
   useEffect(() => {
-    switch (weather.weather[0].icon) {
-      case "01d" || "01n":
-        setWeatherTip(weatherExtra[0].weatherTip);
-        setBackgroundPicture(weatherExtra[0].backgroundPicture);
-        break;
-      case "02d" || "02n":
-        setWeatherTip(weatherExtra[1].weatherTip);
-        setBackgroundPicture(weatherExtra[1].backgroundPicture);
-        break;
-      case "03d" || "03n":
-        setWeatherTip(weatherExtra[2].weatherTip);
-        setBackgroundPicture(weatherExtra[2].backgroundPicture);
-        break;
-      case "04d" || "04n":
-        setWeatherTip(weatherExtra[3].weatherTip);
-        setBackgroundPicture(weatherExtra[3].backgroundPicture);
-        break;
-      case "09d" || "09n":
-        setWeatherTip(weatherExtra[4].weatherTip);
-        setBackgroundPicture(weatherExtra[4].backgroundPicture);
-        break;
-      case "10d" || "10n":
-        setWeatherTip(weatherExtra[5].weatherTip);
-        setBackgroundPicture(weatherExtra[5].backgroundPicture);
-        break;
-      case "11d" || "11n":
-        setWeatherTip(weatherExtra[6].weatherTip);
-        setBackgroundPicture(weatherExtra[6].backgroundPicture);
-        break;
-      case "13d" || "13n":
-        setWeatherTip(weatherExtra[7].weatherTip);
-        setBackgroundPicture(weatherExtra[7].backgroundPicture);
-        break;
-      case "50d" || "50n":
-        setWeatherTip(weatherExtra[8].weatherTip);
-        setBackgroundPicture(weatherExtra[8].backgroundPicture);
-        break;
-
-      default:
-        break;
+    if (
+      weather.weather[0].icon === "01d" ||
+      weather.weather[0].icon === "01n"
+    ) {
+      setWeatherTip(weatherExtra[0].weatherTip);
+      setBackgroundPicture(weatherExtra[0].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "02d" ||
+      weather.weather[0].icon === "02n"
+    ) {
+      setWeatherTip(weatherExtra[1].weatherTip);
+      setBackgroundPicture(weatherExtra[1].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "03d" ||
+      weather.weather[0].icon === "03n"
+    ) {
+      setWeatherTip(weatherExtra[2].weatherTip);
+      setBackgroundPicture(weatherExtra[2].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "04d" ||
+      weather.weather[0].icon === "04n"
+    ) {
+      setWeatherTip(weatherExtra[3].weatherTip);
+      setBackgroundPicture(weatherExtra[3].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "09d" ||
+      weather.weather[0].icon === "09n"
+    ) {
+      setWeatherTip(weatherExtra[4].weatherTip);
+      setBackgroundPicture(weatherExtra[4].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "10d" ||
+      weather.weather[0].icon === "10n"
+    ) {
+      setWeatherTip(weatherExtra[5].weatherTip);
+      setBackgroundPicture(weatherExtra[5].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "11d" ||
+      weather.weather[0].icon === "11n"
+    ) {
+      setWeatherTip(weatherExtra[6].weatherTip);
+      setBackgroundPicture(weatherExtra[6].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "13d" ||
+      weather.weather[0].icon === "13n"
+    ) {
+      setWeatherTip(weatherExtra[7].weatherTip);
+      setBackgroundPicture(weatherExtra[7].backgroundPicture);
+    }
+    if (
+      weather.weather[0].icon === "50d" ||
+      weather.weather[0].icon === "50n"
+    ) {
+      setWeatherTip(weatherExtra[8].weatherTip);
+      setBackgroundPicture(weatherExtra[8].backgroundPicture);
     }
   }, [getWeather]);
 
