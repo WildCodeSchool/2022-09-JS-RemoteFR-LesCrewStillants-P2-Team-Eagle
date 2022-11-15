b195162bbd2dcf46df884d5eb2055d1598166ff7
Alicia-Dupil <dupil.alicia@gmail.com>
2022-11-15 18:47:44 +0100

    Add date functional

--- frontend/src/components/DailyMood.jsx
+++ frontend/src/components/DailyMood.jsx
@@ -1,21 +1,39 @@
 import "./DailyMoodStyle.css";
+import NeutralToast from "../assets/NeutralToast.png";
 
 export default function DailyMood({ dataToasts }) {
+  const date = new Date();
+
   return (
     <section className="DailyMood">
       <div>
         <p className="imgDaily">
           <p>
             <img
-              src={dataToasts[0].picture}
+              src={
+                dataToasts[0].date ===
+                `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+                  ? dataToasts[0].picture
+                  : NeutralToast
+              }
               alt="moodToast"
               className="moodToast"
               width="32px"
             />
           </p>
         </p>
-        <p className="journalToast">{dataToasts[0].note}</p>
-        <p className="dateToast">{dataToasts[0].date}</p>
+        <p className="journalToast">
+          {dataToasts[0].date ===
+          `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+            ? dataToasts[0].note
+            : "Empty Toast"}
+        </p>
+        <p className="dateToast">
+          {dataToasts[0].date ===
+          `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+            ? dataToasts[0].date
+            : `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}
+        </p>
       </div>
     </section>
   );
